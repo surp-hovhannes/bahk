0820acdd44705f67bd425be54fff7b344bb6815a
andar9 <aylitalo@ceevra.com>
2024-01-23 22:01:13 -0800

    [feature] finished setting up skeleton based on Mozilla tutorial

--- bahk/urls.py
+++ bahk/urls.py
@@ -1,5 +1,4 @@
-"""
-URL configuration for bahk project.
+""" URL configuration for bahk project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('hub/', include('hub.urls')),
+    path('', RedirectView.as_view(url='hub/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
