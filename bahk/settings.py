ca942cc6c7b35305fd1347fa4d01b0d8e55e2b92
andar9 <aylitalo@ceevra.com>
2024-01-25 22:16:42 -0800

    working rest framework based on tutorial

--- bahk/settings.py
+++ bahk/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hub.apps.HubConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- hub/views.py
+++ hub/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""Views to perform actions upon API requests."""
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from hub.serializers import GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows user to be viewed or edited."""
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows groups to be viewed or edited."""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
