a174a388e3498081f80ce4347a71d855020e87c8
andar9 <aylitalo@ceevra.com>
2024-02-24 10:38:24 -0800

    [bugfix] rearrange urls to put all hub app urls after hub/ segment (now can access admin page)

--- bahk/urls.py
+++ bahk/urls.py
@@ -13,34 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib import admin
-# from django.urls import include, path
-# from django.views.generic import RedirectView
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('hub/', include('hub.urls')),
-#     path('', RedirectView.as_view(url='hub/')),
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-
-from hub import views
+from django.views.generic import RedirectView
 
 
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-]
-
-urlpatterns += router.urls
+    path('admin/', admin.site.urls),
+    path('hub/', include('hub.urls')),
+    path('', RedirectView.as_view(url='hub/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- hub/urls.py
+++ hub/urls.py
@@ -1,7 +1,8 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from bahk.hub import views
+from hub import views
 
 
 router = routers.DefaultRouter()
