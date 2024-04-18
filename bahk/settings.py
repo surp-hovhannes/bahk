256cd2760c462a8348bb96fec172b22b75c48826
Fr. Mesrop Ash <matt.ash@gmail.com>
2024-04-18 04:46:02 +0000

    Adding changes to use heroku database

--- bahk/settings.py
+++ bahk/settings.py
@@ -76,13 +76,20 @@ WSGI_APPLICATION = 'bahk.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('DATABASE_URL'):
+    # We are running on Heroku, use the DATABASE_URL environment variable
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
