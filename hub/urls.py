2d7efb21c9a6bd22be10bddfb6e7d212bb3534b6
andar9 <aylitalo@ceevra.com>
2024-04-21 01:02:23 -0700

    make home page default when no segment added

--- hub/urls.py
+++ hub/urls.py
@@ -11,8 +11,8 @@ router.register(r"groups", views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path("", views.home, name="home"),
     path("", include(router.urls)),
-    path("home/", views.home, name="home"),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("fast/", views.FastOnDate.as_view(), name="fast_on_date"),
 ]
