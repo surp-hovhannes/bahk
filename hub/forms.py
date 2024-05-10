3fee5bd33c5665016739bafe3b90300a71a133a3
andar9 <aylitalo@ceevra.com>
2024-05-10 10:28:32 -0700

    add form on fast admin page to create fast with date range

--- hub/forms.py
+++ hub/forms.py
@@ -1,15 +1,27 @@
 """Django form classes."""
+from django.core.exceptions import ValidationError
 import django.forms as forms
 
-from hub.models import Church
+from hub.models import Fast
+
+_MAX_FAST_LENGTH = 60  # days
 
 
-class CreateFastWithDatesAdminForm(forms.Form):
-    """Form to allow users to create fast with all its dates through admin interface."""
-    name = forms.CharField(labels="Name of fast", max_length=128)
-    church = forms.ChoiceField(labels="Church", choices=[])
-    description = forms.CharField(label="Description of fast", widget=forms.Textarea())
+class CreateFastWithDatesAdminForm(forms.ModelForm):
+    first_day = forms.DateField(widget=forms.SelectDateWidget)
+    last_day = forms.DateField(widget=forms.SelectDateWidget)
+    class Meta:
+        model = Fast
+        fields = ["name", "church", "description"]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["church"].choices = [(church.name, church.name) for church in Church.objects.all()]
+    def clean_last_day(self):
+        """Ensure last day is not before the first day."""
+        first_day = self.cleaned_data["first_day"]
+        last_day = self.cleaned_data["last_day"]
+        length_of_fast = (last_day - first_day).days + 1
+        if length_of_fast < 1:
+            raise ValidationError(f"Last day ({last_day}) cannot be before the first day ({first_day})")
+        if length_of_fast > _MAX_FAST_LENGTH:
+            raise ValidationError(f"Tried to create a fast lasting {length_of_fast} days. "
+                                  f"Fasts longer than {_MAX_FAST_LENGTH} days are not permitted.")
+        self.cleaned_data["length_of_fast"] = length_of_fast
