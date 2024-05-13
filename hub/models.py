6031b05b7d83e93bc25a878561e618f593c55851
Fr. Mesrop Ash <matt.ash@gmail.com>
2024-05-12 22:06:10 -0700

    Add more_info_url field to Fast model

--- hub/models.py
+++ hub/models.py
@@ -16,30 +16,7 @@ class Fast(models.Model):
     name = models.CharField(max_length=128)
     church = models.ForeignKey(Church, on_delete=models.CASCADE, related_name="fasts")
     description = models.TextField(null=True, blank=True)
-    culmination_feast = models.CharField(max_length=128, null=True, blank=True)
-    culmination_feast_date = models.DateField(unique=True, null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.name} of the {self.church.name}"
-
-
-class Profile(models.Model):
-    """Model for a user profile.
+    more_info_url = models.URLField(max_length=200, blank=True)
     
-    Based on https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    church = models.ForeignKey(Church, null=True, blank=True, on_delete=models.SET_NULL, related_name="profiles")
-    fasts = models.ManyToManyField(Fast, related_name="profiles")
-
     def __str__(self):
-        return self.user.username
-
-
-class Day(models.Model):
-    """Model for a day in time."""
-    date = models.DateField(unique=True)
-    fasts = models.ManyToManyField(Fast, related_name="days")
-
-    def __str__(self):
-        return self.date.strftime("%B-%d-%Y")
+        return f"{{self.name}} of the {{self.church.name}}"
