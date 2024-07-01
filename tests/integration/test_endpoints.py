cd67b07b3277fb1b716bd29c0674fe7ec0021e15
Fr. Mesrop Ash <matt.ash@gmail.com>
2024-07-01 19:42:01 +0000

    Fixes existing tests

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -2,52 +2,99 @@
 import datetime
 
 import pytest
-
 from django.urls import reverse
-from rest_framework.test import APIRequestFactory
-
+from rest_framework.test import APIClient
 from hub import models, views
 
+@pytest.fixture
+def date_query(sample_day_fixture):
+    """Fixture to provide a date query string."""
+    return "?date=" + str(sample_day_fixture.date.strftime("%Y%m%d"))
 
-@pytest.mark.parametrize("query_params", [
-    (""),  # no query params gets fast for today
-    ("?date=20240325"),  # matches sample_day_fixture's date
-])
-@pytest.mark.parametrize("culmination_feast_name", [
-    "Culmination Feast",
-    None,
-])
+
 @pytest.mark.django_db
-def test_fast_on_date_endpoint(query_params, culmination_feast_name, complete_fast_fixture, request):
+@pytest.mark.parametrize("query_params", [
+    "",  # no query params gets fast for today
+    pytest.param("date_query", marks=pytest.mark.usefixtures("date_query"))
+])
+def test_complete_fast_on_date_endpoint(query_params, complete_fast_fixture, sample_user_fixture, sample_church_fixture, date_query):
     """Tests endpoint retrieving fast on a date for a participating user."""
-    view = views.FastOnDate().as_view()
+    
+    client = APIClient()
+    user = sample_user_fixture
+    client.force_authenticate(user=user)
+    
+    complete_fast_fixture.profiles.add(user.profile)
+    
+    days_until_feast = (complete_fast_fixture.culmination_feast_date - datetime.date.today()).days
 
-    user = models.User.objects.filter(profile__fasts=complete_fast_fixture).first()
-    church = models.Church.objects.filter(fasts=complete_fast_fixture).first()
+    countdown = f"<span class='days_to_finish'>{days_until_feast}</span> days until {complete_fast_fixture.culmination_feast}"
+ 
+    url = reverse("fast_on_date") + (date_query if query_params == "date_query" else "")
 
-    # create expected countdown statement
-    countdown = f"1 day until the end of {complete_fast_fixture.name}"
-    if culmination_feast_name is not None:
-        days_until_feast = 2
-        complete_fast_fixture.culmination_feast = culmination_feast_name
-        complete_fast_fixture.culmination_feast_date = datetime.date.today() + datetime.timedelta(days=days_until_feast)
-        complete_fast_fixture.save(update_fields=["culmination_feast", "culmination_feast_date"])
-        countdown = f"{days_until_feast} days until {culmination_feast_name}"
-
-    url = reverse("fast_on_date") + query_params
-
-    factory = APIRequestFactory()
-    request = factory.get(url, format="json")
-    request.user = user
-    response = view(request)
+    response = client.get(url, format="json")
 
     assert response.status_code == 200
-    assert response.data == {
+
+    response_data = {
+        "name": response.data["name"], 
+        "church": response.data["church"],
+        "participant_count": response.data["participant_count"],
+        "description": response.data["description"],
+        "countdown": response.data["countdown"]
+    }
+    
+    expected_data = {
         "name": complete_fast_fixture.name, 
         "church": {
-            "name": church.name
+            "name": sample_church_fixture.name
         },
         "participant_count": complete_fast_fixture.profiles.count(),
         "description": complete_fast_fixture.description,
         "countdown": countdown
     }
+    
+    assert response_data == expected_data
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("query_params", [
+    "",  # no query params gets fast for today
+    pytest.param("date_query", marks=pytest.mark.usefixtures("date_query"))
+])
+def test_incomplete_fast_on_date_endpoint(query_params, incomplete_fast_fixture, sample_user_fixture, sample_church_fixture, date_query):
+    """Tests endpoint retrieving fast on a date for a participating user."""
+    
+    client = APIClient()
+    user = sample_user_fixture
+    client.force_authenticate(user=user)
+    
+    url = reverse("fast_on_date") + (date_query if query_params == "date_query" else "")
+
+    response = client.get(url, format="json")
+
+    assert response.status_code == 200
+
+    days_until_fast_ends = (incomplete_fast_fixture.days.first().date - datetime.date.today()).days +1
+
+    countdown = f"<span class='days_to_finish'>{days_until_fast_ends}</span> days until the end of {incomplete_fast_fixture.name}"
+
+    response_data = {
+        "name": response.data["name"], 
+        "church": response.data["church"],
+        "participant_count": response.data["participant_count"],
+        "description": response.data["description"],
+        "countdown": response.data["countdown"]
+    }
+    
+    expected_data = {
+        "name": incomplete_fast_fixture.name, 
+        "church": {
+            "name": sample_church_fixture.name
+        },
+        "participant_count": incomplete_fast_fixture.profiles.count(),
+        "description": incomplete_fast_fixture.description,
+        "countdown": countdown
+    }
+    
+    assert response_data == expected_data
