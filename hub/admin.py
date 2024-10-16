ea4d7980d74d4a5743beb6ef437dac6fe26eccdc
andar9 <aylitalo@ceevra.com>
2024-10-16 12:01:10 -0700

    [feature] add year field to fast that is autopopulated; allow duplicate fasts if different years; hyperlinked admin fields + migration

--- hub/admin.py
+++ hub/admin.py
@@ -11,32 +11,63 @@ from hub.forms import CreateFastWithDatesAdminForm
 from hub.models import Church, Day, Fast, Profile
 
 
-def _concatenate_queryset(queryset, delim=", ", num=3):
+_MAX_NUM_TO_SHOW = 3  # maximum object names to show in list
+
+
+def _concatenate_queryset(queryset, delim=", ", num=_MAX_NUM_TO_SHOW):
     return delim.join([str(obj) for obj in queryset][:num])
+
+
+def _get_fast_links_url(obj, max_num_to_show=_MAX_NUM_TO_SHOW):
+    num_to_display = min(obj.fasts.all().count(), max_num_to_show)
+
+    args = []
+    is_partial_list = False
+    for i, fast in enumerate(obj.fasts.all()):
+        if i == num_to_display:
+            is_partial_list = True  # not all fasts will be shown
+            break
+        url = reverse("admin:hub_fast_change", args=[fast.pk])
+        args += [url, fast.name]
+    
+    format_string = ", ".join(num_to_display * ['<a href="{}">{}</a>'])
+    # add ellipsis if only partial list of fasts shown
+    format_string += ",..." if is_partial_list else ""
+
+    return format_html(format_string, *args)
 
 
 @admin.register(Church, site=admin.site)
 class ChurchAdmin(admin.ModelAdmin):
-    list_display = ("name", "get_fasts")
+    list_display = ("name", "fast_links",)
     ordering = ("name",)
+    list_display_links = ("name", "fast_links",)
 
-    def get_fasts(self, church):
-        return _concatenate_queryset(church.fasts.all())
+    def fast_links(self, church):
+        return _get_fast_links_url(church)
     
-    get_fasts.short_description = "Fasts"
+    fast_links.short_description = "Fasts"
+
 
 @admin.register(Fast, site=admin.site)
 class FastAdmin(admin.ModelAdmin):
-    list_display = ("get_name", "church", "get_days", "culmination_feast", "culmination_feast_date", "description", "image",
+    list_display = ("get_name", "church_link", "get_days", "culmination_feast", "culmination_feast_date", "description", "image",
                     "url_link",)
-    list_display_links = ("url_link",)
-    ordering = ("church", "name",)
+    list_display_links = ("get_name", "church_link", "url_link",)
+    ordering = ("-year", "church", "name",)
 
     def get_name(self, fast):
-        year = fast.days.first().date.year
-        return f"{fast.name} ({year})"
+        return str(fast)
     
     get_name.short_description = "Fast Name"
+
+    def church_link(self, fast):
+        if not fast.church:
+            return ""
+        url = reverse("admin:hub_church_change", args=[fast.church.pk])
+        return format_html('<a href="{}">{}</a>', url, fast.church.name)
+    
+    church_link.short_description = "Church"
     
     def get_days(self, fast):
         return _concatenate_queryset(fast.days.all())
@@ -48,7 +79,7 @@ class FastAdmin(admin.ModelAdmin):
             return ""
         return format_html('<a href="{}">{}</a>', fast.url, fast.url)
     
-    url_link.short_descrption = "Link to Learn More"
+    url_link.short_description = "Link to Learn More"
 
     def get_urls(self):
         """Add endpoints to admin views."""
@@ -91,16 +122,41 @@ class FastAdmin(admin.ModelAdmin):
 
 @admin.register(Profile, site=admin.site)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "church", "get_fasts", "location", "receive_upcoming_fast_reminders", "profile_image", )
+    list_display = ("user", "church_link", "fast_links", "location", "receive_upcoming_fast_reminders", "profile_image", )
+    list_display_links = ("user", "church_link", "fast_links",)
     ordering = ("church", "user",)
 
-    def get_fasts(self, profile):
-        return _concatenate_queryset(profile.fasts.all())
+    def church_link(self, fast):
+        if not fast.church:
+            return ""
+        url = reverse("admin:hub_church_change", args=[fast.church.pk])
+        return format_html('<a href="{}">{}</a>', url, fast.church.name)
     
-    get_fasts.short_description = "Fasts"
+    church_link.short_description = "Church"
+
+    def fast_links(self, profile):
+        return _get_fast_links_url(profile)
+    
+    fast_links.short_description = "Fasts"
 
 
 @admin.register(Day, site=admin.site)
 class DayAdmin(admin.ModelAdmin):
-    list_display = ("date", "church", "fast",)
+    list_display = ("date", "church_link", "fast_link",)
     ordering = ("church", "date",)
+
+    def church_link(self, day):
+        if not day.fast or not day.fast.church:
+            return ""
+        url = reverse("admin:hub_church_change", args=[day.fast.church.pk])
+        return format_html('<a href="{}">{}</a>', url, day.fast.church.name)
+
+    church_link.short_description = "Church"
+
+    def fast_link(self, day):
+        if not day.fast:
+            return ""
+        url = reverse("admin:hub_fast_change", args=[day.fast.pk])
+        return format_html('<a href="{}">{}</a>', url, day.fast.name)
+    
+    fast_link.short_description = "Fast"
