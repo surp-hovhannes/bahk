66b79f1bc674f656e81a25480e78bb44b0a6123a
andar9 <aylitalo@ceevra.com>
2024-11-10 20:25:18 -0800

    skip appended readings; lint

--- hub/management/commands/import_readings.py
+++ hub/management/commands/import_readings.py
@@ -9,6 +9,10 @@ from django.core.management.base import BaseCommand
 import hub.models as models
 
 
+MAX_NUM_READINGS = 40
+PARSER_REGEX = r"^([A-za-z1-4\'\. ]+) ([0-9]+\.)?([0-9]+)\-?([0-9]+\.)?([0-9]+)?$"
+
+
 def daterange(start_date: date, end_date: date):
     days = int((end_date - start_date).days)
     for n in range(days):
@@ -17,7 +21,7 @@ def daterange(start_date: date, end_date: date):
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("--church", help="name of church to add reading to their calendar")
+        parser.add_argument("--church", required=True, help="name of church to add reading to their calendar")
         parser.add_argument('--start_date', default=date.today().strftime("%Y-%m-%d"), help="date to start importing readings")
         parser.add_argument('--end_date', default=(date.today() + timedelta(10)).strftime("%Y-%m-%d"),
                             help="date to end importing readings")
@@ -50,44 +54,46 @@ class Command(BaseCommand):
 
             book_start = html_content.find("<b>")
             
+            ct = 0
             while book_start != -1:
+                # prevent infinite loop
+                if ct > MAX_NUM_READINGS:
+                    logging.error("Reached maximum number of readings. Breaking to avoid infinite loop.")
+                    break
+                ct += 1
+
                 i1 = book_start + len("<b>")
                 i2 = html_content.find("</b>")
                 reading_str = html_content[i1:i2]
-                # advance to next section of web text to prevent infinite loop
+
+                # advance to next section of web text early to prevent infinite loop in case of failure later on
                 html_content = html_content[i2 + 1:]
                 book_start = html_content.find("<b>")
 
                 if "," in reading_str:
-                    # rare occasion where two readings are merged (Dnaniel 3.1-23 and Azariah 1-68)
-                    reading, _ = models.Reading.objects.get_or_create(
-                        day=day,
-                        book="Azariah",
-                        start_chapter=1,
-                        start_verse=1,
-                        end_chapter=1,
-                        end_verse=68,
-                    )
+                    # TODO: if comma found, second reading appended to first, as for Daniel 3.1-23 and Azariah 1-68
+                    # for now, omit second reading
                     reading_str = reading_str.split(",")[0]
-
-                parser_regex = r"^([A-za-z1-4\'\. ]+) ([0-9]+)\.([0-9]+)\-?([0-9]+\.)?([0-9]+)?$"
-                groups = re.search(parser_regex, reading_str)
+                
+                groups = re.search(PARSER_REGEX, reading_str)
 
                 # skip reading if does not match parser regex
                 if groups is None:
-                    logging.error("Could not parse reading %s at %s with regex %s", reading_str, url, parser_regex)
+                    logging.error("Could not parse reading %s at %s with regex %s", reading_str, url, PARSER_REGEX)
                     continue
 
                 try:
                     # parse groups
                     book = groups.group(1)
-                    start_chapter = groups.group(2)
+                    # remove decimal if start chapter provided; otherwise, part of book with 1 chapter
+                    start_chapter = groups.group(2).strip(".") if groups.group(2) is not None else 1
                     start_verse = groups.group(3)
-                    end_chapter = groups.group(4).strip(".") if groups.group(4) is not None else start_chapter  # rm decimal
+                    # remove decimal if end chapter provided; otherwise, must be the same as the start chapter
+                    end_chapter = groups.group(4).strip(".") if groups.group(4) is not None else start_chapter
                     end_verse = groups.group(5) if groups.group(5) is not None else start_verse
 
                     day, _ = models.Day.objects.get_or_create(church=church, date=single_date)
-                    reading, _ = models.Reading.objects.get_or_create(
+                    models.Reading.objects.get_or_create(
                         day=day,
                         book=book,
                         start_chapter=int(start_chapter),
@@ -95,7 +101,9 @@ class Command(BaseCommand):
                         end_chapter=int(end_chapter),
                         end_verse=int(end_verse),
                     )
-                except Exception as e:
-                    logging.error("Could not parse reading with text %s with regex %s from %s. Skipping.", 
-                                reading_str, parser_regex, url, exc_info=True)
+                except Exception:
+                    logging.error(
+                        "Could not parse reading with text %s with regex %s from %s. Skipping.", 
+                        reading_str, PARSER_REGEX, url, exc_info=True
+                    )
                     continue
