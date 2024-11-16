3083cc88b078765f2cdb25010ee9adf288706399
Fr. Mesrop Ash <matt.ash@gmail.com>
2024-11-16 04:45:49 +0000

    update to serializer for device type

--- notifications/views.py
+++ notifications/views.py
@@ -5,12 +5,23 @@ from rest_framework.response import Response
 from .models import DeviceToken
 from .serializers import DeviceTokenSerializer
 from .utils import send_push_notification
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 
 class DeviceTokenCreateView(generics.CreateAPIView):
     queryset = DeviceToken.objects.all()
     serializer_class = DeviceTokenSerializer
+
+    def create(self, request, *args, **kwargs):
+        logger.info(f"Received request data: {request.data}")
+        return super().create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        logger.info(f"Saving device token with data: {serializer.validated_data}")
+        serializer.save()
 
 class TestPushNotificationView(APIView):
     def post(self, request):
