f1b21485e1ce067f1cfb241548ea6cbe41b04fa1
Fr. Mesrop Ash <matt.ash@gmail.com>
2024-11-18 16:49:46 +0000

    Adds Learning Resources App

--- bahk/settings.py
+++ bahk/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'app_management',
     'markdownx',
     'corsheaders',
+    'learning_resources',
 ]
 
 MIDDLEWARE = [
---
+++ learning_resources/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Video, Article
+
+class VideoSerializer(serializers.ModelSerializer):
+    thumbnail_small_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Video
+        fields = [
+            'id', 'title', 'description', 'thumbnail', 
+            'thumbnail_small_url', 'video', 'created_at', 
+            'updated_at'
+        ]
+        read_only_fields = ['created_at', 'updated_at']
+
+    def get_thumbnail_small_url(self, obj):
+        if obj.thumbnail_small:
+            return obj.thumbnail_small.url
+        return None
+
+class ArticleSerializer(serializers.ModelSerializer):
+    thumbnail_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Article
+        fields = [
+            'id', 'title', 'body', 'image', 
+            'thumbnail_url', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['created_at', 'updated_at']
+
+    def get_thumbnail_url(self, obj):
+        if obj.thumbnail:
+            return obj.thumbnail.url
+        return None 
