3eac3d037cd6eace2db391aca9f668fc480c63ef
andar9 <aylitalo@ceevra.com>
2024-12-28 18:46:17 -0800

    [bugfix] return catena home page url if no url is created (back up to avoid server error that causes all reading fetching to fail)

--- hub/constants.py
+++ hub/constants.py
@@ -2,6 +2,7 @@
 NUMBER_PARTICIPANTS_TO_SHOW_WEB = 6  # number of participant thumbnails to show on fast card on web version
 DATE_FORMAT_STRING = "%m/%d/%Y"  # format string for dates (month, day, year)
 
+CATENA_HOME_PAGE_URL = "https://catenabible.com/"
 CATENA_ABBREV_FOR_BOOK = {
     # Old Testament
     "Genesis": "gn",
