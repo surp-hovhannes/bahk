f9a2f78bf4c151b3413e6054fb7d70e07c3d2597
mattash <matt.ash@gmail.com>
2025-04-15 22:26:22 +0000

    Unnecessary to add notifiaction urls here. They are already associated with /api/* route.

--- bahk/urls.py
+++ bahk/urls.py
@@ -62,11 +62,6 @@ urlpatterns += [
     path('api/s3-upload/', include('s3_file_field.urls')),
 ]
 
-# Notifications URLs
-urlpatterns += [
-    path('notifications/', include('notifications.urls')),
-]
-
 # for serving media files during development
 if not settings.IS_PRODUCTION:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
